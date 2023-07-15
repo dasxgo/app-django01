b0d057729aa8751676f4ff713f59cbfd320127fe
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-07-15 12:41:08 -0500

    actualizacion codigo

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
 
 # Create your views here.
 
-from django.http import HttpResponse
-from .models import Question
 
 def index(request): 
     latest_question_list = Question.objects.all()
@@ -21,6 +22,16 @@ def results(request, question_id):
     return HttpResponse(f"Estas viendo los resultados de la pregunta numero{question_id}")
 
 def vote(request, question_id):
-    return HttpResponse(f"Estas votando a la pregunta numero{question_id}")
-
+    question = get_object_or_404(Question, pk=question_id)
+    try: 
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(keyError, Choice.DoesNotExits): 
+        return render(request, 'polls/detail.html', {
+            'question' : question,
+            'error_message' : 'No elegiste una respuesta'
+        })
+    else:
+        slected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
    
