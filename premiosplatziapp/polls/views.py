e9255b2b307d019abb1c393f36134dac015faef3
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-07-15 16:35:35 -0500

    actualizacion de codigo

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -30,14 +30,14 @@ def detail(request, question_id):
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-
+    
     def get_queryset(self):
-        '''Return the last five published questions'''
-        return Question.objects.order_by('-pub_date')[:5]
+        """Returns the last five published questions"""
+        return Question.objects.order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'polls/details.html'
+    template_name = 'polls/detail.html'
 
 class ResultView(DetailView):
     model = Question
@@ -47,23 +47,13 @@ def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    
     except (KeyError, Choice.DoesNotExist):
-        template = 'polls/detail.html'
-        context = {
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': 'No elegiste una respuesta',
-        }
-        return render(request, template, context)
-    
+        })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        
-        viewname = 'polls:results'
-        context = {
-            'pk': question.id
-        }
-        
-        return HttpResponseRedirect(reverse(viewname, kwargs=context))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
