7c166421a96917de189cc5ab73adf9480f50c028
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-07-15 13:13:58 -0500

    se arregla errores en el codigo views

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question, Choice
 from django.urls import reverse
+
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -23,15 +24,15 @@ def results(request, question_id):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    try: 
+    try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except(keyError, Choice.DoesNotExits): 
-        return render(request, 'polls/detail.html', {
-            'question' : question,
-            'error_message' : 'No elegiste una respuesta'
+    except(KeyError, Choice.DoesNotExist):
+        return render(request,"polls/detail.html",{
+            'question': question,
+            'error_message':"No elegiste una respuesta"
         })
     else:
-        slected_choice.votes += 1
+        selected_choice.votes +=1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
-   
+        return HttpResponseRedirect(reverse("polls:results",args = (question_id,)))
+
