3d49f1d50c9c05f74f33ce04041838d6fa3138bd
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-07-15 09:12:20 -0500

    se adiciona template

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -12,7 +12,10 @@ def index(request):
     })
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta numero{question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {
+        'question' : question
+    })
 
 def results(request, question_id):
     return HttpResponse(f"Estas viendo los resultados de la pregunta numero{question_id}")
