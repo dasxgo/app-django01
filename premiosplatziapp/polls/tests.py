7cd411fd7a18e030ee11a0cbc253356db28815f0
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-07-16 10:45:29 -0500

    actualizacion de codigo models

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,3 +1,20 @@
+import datetime
+
+
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
 
 # Create your tests here.
+
+# Modelos
+# Vistas
+
+class QuestionModelTest(TestCase): 
+
+    def test_was_published_recently_with_future_questions(self):
+        """"was_published_recently returns False for questions whose pub_datew in inthe future"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text='Quen es el mejor Course Director de Platzi?', pub_date=time)
+        self.assertIS(future_question.was_published_recently(), False)
