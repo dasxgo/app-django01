4a10015809f0c19268e1fe720aee7e96033b7e3f
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-07-16 16:47:58 -0500

    se adiciona codigos test

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,6 +1,5 @@
 import datetime
 
-
 from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
@@ -32,10 +31,29 @@ class QuestionModelTest(TestCase):
         present_question = Question(question_text="¿Quien es el mejor Course Direct de Platzi?",pub_date=time)
         self.assertIs(present_question.was_published_recently(),True)   
 
+def create_question(question_text, days):
+    ''' Create a question with the given 'question_tect', and published the given number of days offset to now
+        (negative for questions published in the past, positive foer questions tant have yet to be published)'''
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
 class QuestionIndexViews(TestCase):
     def test_no_question(self):
         '''if no question exist, an appropieta message is displated'''
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'No polls are available') 
-        self.assertQuerysetEqual(response.context['latest_question_list'],[])                           
+        self.assertQuerysetEqual(response.context['latest_question_list'],[]) 
+
+    def test_future_question(self):
+        '''Questions with a pub_date in the future aren't displayed on the index page'''
+        create_question('Future question', days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, 'No polls are available')
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+        
+        def test_past_question(self):
+            ''' Question wiht a pub_date in the past are displayed on the index page'''
+        question = create_question("Past question", days=-10)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(response.context["latest_question_list"],[question])                   
